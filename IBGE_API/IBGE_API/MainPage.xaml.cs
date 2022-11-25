086d1facd285887ba23dbcbc26b2e9834b50cdf8
jose.rufino01 <jose.rufino01@A10M07>
2022-11-25 15:12:03 -0300

    Adicionar arquivos de projeto.

---
+++ IBGE_API/IBGE_API/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using IBGE_API.Models;
+using System.Collections.ObjectModel;
+
+namespace IBGE_API {
+    public partial class MainPage : ContentPage {
+        public MainPage()
+        {
+            InitializeComponent();
+            OnLoad();
+        }
+
+        private const string Url = "https://servicodados.ibge.gov.br/api/v1/" + 
+            "localidades/estados";
+        private readonly HttpClient _client = new HttpClient();
+        private ObservableCollection<Estados> _estados;
+
+        private async void OnLoad()
+        {
+            string Estados = await _client.GetStringAsync(Url);
+            List<Estados> posts = JsonConvert.DeserializeObject<List<Estados>>(Estados);
+            _estados = new ObservableCollection<Estados>(posts);
+            lvEstados.ItemsSource = _estados;
+        }
+
+        private void PickerSelect(object sender, EventArgs e)
+        {
+            var selecao = pkEstados.Items[pkEstados.SelectedIndex];
+
+            
+        }
+    }
+}
---
+++ IBGE_API/IBGE_API/Models/Estados.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBGE_API.Models {
+    public class Rootobject {
+        public Estados[] Property1 { get; set; }
+    }
+
+    public class Estados {
+        public int Id { get; set; }
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+        public Regiao Regiao { get; set; }
+    }
+
+    public class Regiao {
+        public int Id { get; set; }
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+    }
+}
